859cc78220fcebe26e122929bc8405df165f5f15
wuhou <78024072+wuhou187@users.noreply.github.com>
2024-05-22 17:58:20 +0800

    Update Collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,7 +3,7 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Buggy implementation of nextNumber! */git 
     public static int nextNumber(int n) {
         if (n  == 128) {
             return 1;
